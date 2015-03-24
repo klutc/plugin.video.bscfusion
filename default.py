8c571ab0cbc1ac0be64a9aef72b5c4e9a41ea3a1
kodi1 <kodi.dev.one@gmail.com>
2015-03-24 13:17:28 +0200

    Initial commit
    	Used AES module https://github.com/ricmoo/pyaes

---
+++ default.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+import os, sys
+import re
+
+import xbmc
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+import xbmcvfs
+
+import urllib, time
+from datetime import datetime, timedelta
+from ga import ga
+
+__addon__ = xbmcaddon.Addon()
+__author__ = __addon__.getAddonInfo('author')
+__scriptid__ = __addon__.getAddonInfo('id')
+__scriptname__ = __addon__.getAddonInfo('name')
+__version__ = __addon__.getAddonInfo('version')
+__icon__ = __addon__.getAddonInfo('icon').decode("utf-8")
+__language__ = __addon__.getLocalizedString
+__cwd__ = xbmc.translatePath( __addon__.getAddonInfo('path') ).decode("utf-8")
+__profile__ = xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode("utf-8")
+__resource__ = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) ).decode("utf-8")
+__icon_msg__ = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'bulsat.png' ) ).decode("utf-8")
+__data__ = xbmc.translatePath( os.path.join( __profile__, '', 'data.dat') ).decode("utf-8")
+sys.path.insert(0, __resource__)
+
+def Notify (msg1, msg2):
+  xbmc.executebuiltin((u'Notification(%s,%s,%s,%s)' % (msg1, msg2, '5000', __icon_msg__)).encode('utf-8'))
+
+if __addon__.getSetting("firstrun") == "true":
+  Notify('Settings', 'empty')
+  __addon__.openSettings()
+  __addon__.setSetting("firstrun", "false")
+
+refresh = __addon__.getSetting("refresh")
+timeout = __addon__.getSetting("timeout")
+base = __addon__.getSetting("base")
+offset = int(__addon__.getSetting("offset"))
+
+if __addon__.getSetting("dbg") == 'true':
+  dbg = True
+else:
+  dbg = False
+
+if not __addon__.getSetting("username"):
+  Notify('User', 'empty')
+if not __addon__.getSetting("password"):
+  Notify('Password', 'empty')
+
+def update(name, dat, crash=None):
+  payload = {}
+  payload['an'] = __scriptname__
+  payload['av'] = __version__
+  payload['ec'] = name
+  payload['ea'] = 'tv_start'
+  payload['ev'] = '1'
+  payload['dl'] = urllib.quote_plus(dat.encode('utf-8'))
+  ga().update(payload, crash)
+
+def timesmk(v):
+  ts = v.get('start', None)
+  te = v.get('stop', None)
+  if ts is None or te is None:
+    return u''
+  ts = datetime.fromtimestamp(time.mktime(time.strptime(ts.split()[0], '%Y%m%d%H%M%S'))) + timedelta(minutes=offset)
+  te = datetime.fromtimestamp(time.mktime(time.strptime(te.split()[0], '%Y%m%d%H%M%S'))) + timedelta(minutes=offset)
+  return u'%s %s' % (ts.strftime("%H:%M:%S"), te.strftime("%H:%M:%S"))
+
+def indexch():
+  import traceback
+  try:
+    import bsc
+    b = bsc.dodat(base = base,
+                  login = {'usr': __addon__.getSetting("username"),
+                          'pass': __addon__.getSetting("password")
+                          },
+                  cachepath = __data__,
+                  cachetime = refresh,
+                  dbg = dbg,
+                  timeout=float(timeout))
+  except Exception, e:
+    Notify('Module Import', 'Fail')
+    update('exception', e.args[0], sys.exc_info())
+    traceback.print_exc()
+    pass
+
+  try:
+    i = -1
+    c = {}
+    for i, c in b.data_fetch():
+      addch(i, c)
+    Notify('Data', 'Fetch Ok')
+  except Exception, e:
+    if e.args[0] == 'LoginFail':
+      Notify('LoginFail', 'Check login data')
+    else:
+      Notify('Data', 'Fetch Fail')
+    update('exception', '%s:%d->%s' % (e.args[0], i, c.get('title', None)), sys.exc_info())
+    traceback.print_exc()
+    pass
+
+def playch(url, name):
+  li = xbmcgui.ListItem(path=url)
+  # li.addStreamInfo('video', { 'duration': 3600})
+  # li.addStreamInfo('subtitle', { 'language': 'en' })
+  xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, li)
+  update(name, url)
+
+def addch(idx, dat):
+  u = sys.argv[0] + "?url=" + urllib.quote_plus(dat['sources']) + "&mode=" + str(1) + "&name=" + urllib.quote_plus(dat['title'].encode('utf-8'))
+  liz = xbmcgui.ListItem(dat['title'], iconImage=dat['logo_mobile_selected'], thumbnailImage=dat['logo_mobile'])
+  info = u'%s - Ch: %d' % (timesmk(dat), idx)
+  for c in ('program', 'desc', 'channel', 'pg'):
+    d = dat.get(c, ' ')
+    if d is not None:
+      info += ' ' + d
+
+  liz.setInfo(type="video", infoLabels={"Title": dat['title'], "plot": ' '.join(info.split())})
+  liz.setInfo('video', { 'title': name})
+  liz.setProperty('fanart_image', dat['logo_epg'])
+  liz.setProperty("IsPlayable" , "true")
+  return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=False)
+
+def setviewmode():
+  if (xbmc.getSkinDir() != "skin.confluence") or __addon__.getSetting("viewset") != 'true':
+    return
+  mode = {
+            '0': '52',
+            '1': '502',
+            '2': '51',
+            '3': '500',
+            '4': '501',
+            '5': '508',
+            '6': '504',
+            '7': '503',
+            '8': '515'
+          }
+  xbmc.executebuiltin('Container.SetViewMode(%s)' % mode[__addon__.getSetting("viewmode")])
+
+def get_params():
+  param = []
+  paramstring = sys.argv[2]
+  if len(paramstring) >= 2:
+    params = sys.argv[2]
+    cleanedparams = params.replace('?', '')
+    if (params[len(params) - 1] == '/'):
+      params = params[0:len(params) - 2]
+    pairsofparams = cleanedparams.split('&')
+    param = {}
+    for i in range(len(pairsofparams)):
+      splitparams = {}
+      splitparams = pairsofparams[i].split('=')
+      if (len(splitparams)) == 2:
+        param[splitparams[0]] = splitparams[1]
+    return param
+
+params = get_params()
+url = None
+name = None
+mode = None
+
+try:
+  url = urllib.unquote_plus(params["url"])
+except:
+  pass
+try:
+  name = urllib.unquote_plus(params["name"])
+except:
+  pass
+try:
+  mode = int(params["mode"])
+except:
+  pass
+
+if mode == None or url == None or len(url) < 1:
+  indexch()
+elif mode == 1:
+  playch(url, name)
+
+xbmcplugin.setContent(int(sys.argv[1]), 'movies')
+setviewmode()
+xbmcplugin.endOfDirectory(int(sys.argv[1]), cacheToDisc=True)
---
+++ resources/lib/bsc.py
@@ -0,0 +1,142 @@
+# -*- coding: utf8 -*-
+
+import os, json, time
+import base64
+from requests import Session, codes
+import io
+from time import localtime, strftime
+import aes as EN
+
+class dodat():
+  def __init__(self,
+                base,
+                login,
+                cachepath,
+                cachetime=1,
+                dbg=False,
+                dump_name='',
+                timeout=0.5):
+    self.__UA = {
+                'Host': 'api.iptv.bulsat.com',
+                'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:36.0) Gecko/20100101 Firefox/36.0',
+                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate',
+                'Referer': 'https://test.iptv.bulsat.com/iptv.php',
+                'Origin': 'https://test.iptv.bulsat.com',
+                'Connection': 'keep-alive',
+                'Pragma': 'no-cache',
+                }
+
+    self.__log_in = {}
+    self.__p_data = {
+                'user' : ['',''],
+                'device_id' : ['','pcweb'],
+                'device_name' : ['','pcweb'],
+                'os_version' : ['','pcweb'],
+                'os_type' : ['','pcweb'],
+                'app_version' : ['','0.01'],
+                'pass' : ['',''],
+                }
+    self.__cachepath = cachepath
+    self.__refresh = int(cachetime) * 60
+    self.__p_data['user'][1] = login['usr']
+    self.__log_in['pw'] = login['pass']
+    self.__DEBUG_EN = dbg
+    self.__t = timeout
+    self.__BLOCK_SIZE = 16
+    self.__URL_LOGIN = base + '/?auth'
+    self.__URL_LIST = base + '/?json&tv&mini'
+    self.__js = None
+
+  def __log_dat(self, d):
+    if self.__DEBUG_EN is not True:
+      return
+    print '---------'
+    if type(d) is str:
+      print d
+    else:
+      for k, v in d.iteritems():
+        print k + ' : ' + str(v)
+
+  def __store_data(self):
+      with io.open(self.__cachepath, 'w+', encoding=self.__char_set) as f:
+        f.write(unicode(json.dumps(self.__js,
+                        sort_keys = True,
+                        indent = 1,
+                        ensure_ascii=False)))
+
+  def __restore_data(self):
+    with open(self.__cachepath, 'r') as f:
+      self.__js = json.load(f)
+
+  def __goforit(self):
+    s = Session()
+    r = s.post(self.__URL_LOGIN, timeout=self.__t,
+                headers=self.__UA)
+
+    if r.status_code == codes.ok:
+      self.__log_in['key'] = r.headers['challenge']
+      self.__log_in['session'] = r.headers['ssbulsatapi']
+
+      s.headers.update({'SSBULSATAPI': self.__log_in['session']})
+
+      _text = self.__log_in['pw'] + (self.__BLOCK_SIZE - len(self.__log_in['pw']) % self.__BLOCK_SIZE) * '\0'
+
+      enc = EN.AESModeOfOperationECB(self.__log_in['key'])
+      self.__p_data['pass'][1] = base64.b64encode(enc.encrypt(_text))
+
+      self.__log_dat(self.__log_in)
+      self.__log_dat(self.__p_data)
+
+      r = s.post(self.__URL_LOGIN, timeout=self.__t,
+                  headers=self.__UA, files=self.__p_data)
+
+      self.__log_dat(r.request.headers)
+      self.__log_dat(r.request.body)
+
+      if r.status_code == codes.ok:
+        data = r.json()
+        if data['Logged'] == 'true':
+          self.__log_dat('Login ok')
+          s.headers.update({'Access-Control-Request-Method': 'POST'})
+          s.headers.update({'Access-Control-Request-Headers': 'ssbulsatapi'})
+
+          r = s.options(self.__URL_LIST, timeout=self.__t,
+                          headers=self.__UA)
+
+          self.__log_dat(r.request.headers)
+          self.__log_dat(r.headers)
+          self.__log_dat(str(r.status_code))
+
+          r = s.post(self.__URL_LIST, timeout=self.__t,
+                      headers=self.__UA)
+
+          self.__log_dat(r.request.headers)
+          self.__log_dat(r.headers)
+          if r.status_code == codes.ok:
+            self.__char_set = r.headers['content-type'].split('charset=')[1]
+            self.__log_dat('get data ok')
+            self.__js = r.json()
+            self.__log_dat(self.__js)
+            self.__store_data()
+        else:
+          raise Exception("LoginFail")
+
+  def data_fetch(self):
+    if os.path.exists(self.__cachepath):
+      self.__restore_data()
+      if time.time() - self.__js['ts'] < self.__refresh:
+        self.__log_dat('Use cache file')
+      else:
+        self.__log_dat('Use site')
+        self.__js = None
+
+    if self.__js is None:
+      self.__goforit()
+      self.__js['ts'] = divmod(time.time(), self.__refresh)[0] * self.__refresh
+      self.__log_dat('Base time: %s' % time.ctime(self.__js['ts']))
+      self.__store_data()
+
+    for ch in self.__js['tv']:
+      yield self.__js['tv'].index(ch) , ch
